472de03486c8bb8dcf8445aedd4423a9e9dcf8fa
Eric Faurie <efaurie@hawk.iit.edu>
2015-06-04 15:33:15 -0500

    Add blank analytics pane

---
+++ src/org/wincom/gui/WinRetrieveAnalysisPane.java
@@ -0,0 +1,10 @@
+package org.wincom.gui;
+
+import javax.swing.*;
+
+public class WinRetrieveAnalysisPane extends JPanel {
+
+    public WinRetrieveAnalysisPane() {
+
+    }
+}
--- src/org/wincom/gui/WinRetrieveTabPane.java
+++ src/org/wincom/gui/WinRetrieveTabPane.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class WinRetrieveTabPane extends JTabbedPane {
     private WinRetrieveQueryPane queryPane;
     private WinRetrieveDebugPane debugPane;
+    private WinRetrieveAnalysisPane analysisPane;
 
     public WinRetrieveTabPane(FileRetrievalAPI fileDownloaderAPI) {
         initComponents(fileDownloaderAPI);
@@ -16,10 +17,12 @@ public class WinRetrieveTabPane extends JTabbedPane {
     private void initComponents(FileRetrievalAPI fileDownloaderAPI) {
         queryPane = new WinRetrieveQueryPane(fileDownloaderAPI);
         debugPane = new WinRetrieveDebugPane();
+        analysisPane = new WinRetrieveAnalysisPane();
     }
 
     private void initTabs() {
         this.addTab("Query", queryPane);
+        this.addTab("Analysis", analysisPane);
         this.addTab("Debug", debugPane);
     }
 }
